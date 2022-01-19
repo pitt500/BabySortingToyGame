30a4699816087815393825df28f7583dac91671a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-19 10:40:06 -0700

    Add BabyToyView

---
+++ BabySortingToyGame/BabySortingToyGame/Views/BabyToyView.swift
@@ -0,0 +1,20 @@
+//
+//  BabyToyView.swift
+//  BabySortingToyGame
+//
+//  Created by Pedro Rojas on 19/01/22.
+//
+
+import SwiftUI
+
+struct BabyToyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BabyToyView_Previews: PreviewProvider {
+    static var previews: some View {
+        BabyToyView()
+    }
+}
