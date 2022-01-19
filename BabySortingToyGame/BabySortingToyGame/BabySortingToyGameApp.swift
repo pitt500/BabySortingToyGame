45181c159178677394fd1fe2eab765b40ebdf0d1
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-19 10:30:28 -0700

    New project

---
+++ BabySortingToyGame/BabySortingToyGame/BabySortingToyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySortingToyGameApp.swift
+//  BabySortingToyGame
+//
+//  Created by Pedro Rojas on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BabySortingToyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
