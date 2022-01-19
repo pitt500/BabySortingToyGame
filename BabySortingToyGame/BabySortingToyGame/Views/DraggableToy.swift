942933a9f34a39d09e01ae70cf7c5537ad483378
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-19 10:46:07 -0700

    Add Draggable Toy

---
+++ BabySortingToyGame/BabySortingToyGame/Views/DraggableToy.swift
@@ -0,0 +1,20 @@
+//
+//  DraggableToy.swift
+//  BabySortingToyGame
+//
+//  Created by Pedro Rojas on 19/01/22.
+//
+
+import SwiftUI
+
+struct DraggableToy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DraggableToy_Previews: PreviewProvider {
+    static var previews: some View {
+        DraggableToy()
+    }
+}
