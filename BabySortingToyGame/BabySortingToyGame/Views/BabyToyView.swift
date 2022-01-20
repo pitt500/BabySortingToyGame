cab3917bf134cfa61b6afad576fcd897b1274b60
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-19 17:21:04 -0700

    Add a ZStack for all the objects

--- BabySortingToyGame/BabySortingToyGame/Views/BabyToyView.swift
+++ BabySortingToyGame/BabySortingToyGame/Views/BabyToyView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct BabyToyView: View {
     let currentToy = Toy(id: 1, color: .red)
     @State var position = CGPoint(x: 100, y: 100)
+    let gridItems = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
     
     var drag: some Gesture {
         DragGesture()
@@ -22,11 +27,20 @@ struct BabyToyView: View {
     }
     
     var body: some View {
-        DraggableToy(
-            toy: currentToy,
-            position: position,
-            gesture: drag
-        )
+        ZStack {
+            LazyVGrid(columns: gridItems, spacing: 100) {
+                ForEach(0..<6, id: \.self) { _ in
+                    Circle()
+                        .fill(.blue)
+                        .frame(width: 100, height: 100)
+                }
+            }
+            DraggableToy(
+                toy: currentToy,
+                position: position,
+                gesture: drag
+            )
+        }
     }
 }
 
